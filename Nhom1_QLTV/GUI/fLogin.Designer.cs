d878e95845eac67e018db04dade5c906b1393d27
hien.van.nguyen0408@gmail.com <xamlone321@gmail.com>
2021-03-05 00:00:25 +0700

    them gitignore

--- Nhom1_QLTV/GUI/fLogin.Designer.cs
+++ Nhom1_QLTV/GUI/fLogin.Designer.cs
@@ -14,6 +14,7 @@ namespace Nhom1_QLTV.GUI
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
+
             if (disposing && (components != null))
             {
                 components.Dispose();
